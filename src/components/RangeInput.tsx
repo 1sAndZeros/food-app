cc47c4fe19eb646f5630a573b791532470cc8a82
1sAndZeros <rikie_p@hotmail.com>
2024-03-24 20:25:07 +0000

    Refactor RangeInput component styling

--- src/components/RangeInput.tsx
+++ src/components/RangeInput.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 
-const Input = styled.input`
+const RangeInput = styled.input.attrs({ type: 'range' })`
   -webkit-appearance: none; /* Hides the slider so that custom slider can be made */
   appearance: none; /* Hides the slider so that custom slider can be made */
   margin: 1rem 0;
@@ -11,14 +11,13 @@ const Input = styled.input`
   /* Special styling for WebKit/Blink */
   &::-webkit-slider-thumb {
     -webkit-appearance: none;
-    border: 1px solid black;
+    border: none;
     height: 16px;
     width: 16px;
     border-radius: 100%;
     background: ${(props) => props.theme.colors.primary};
     cursor: pointer;
     margin-top: -6px; /* You need to specify a margin in Chrome, but in Firefox and IE it is automatic */
-    box-shadow: 1px 1px 1px black, 0px 0px 1px #0d0d0d; /* Add cool effects to your sliders! */
   }
 
   /* All the same stuff for Firefox */
@@ -35,25 +34,13 @@ const Input = styled.input`
     outline: none; /* Removes the blue border. You should probably do some kind of focus styling for accessibility reasons though. */
   }
 
-  &::-ms-track {
-    width: 100%;
-    cursor: pointer;
-    /* Hides the slider so custom styles can be added */
-    background: transparent;
-    border-color: transparent;
-    color: transparent;
-  }
-
   &::-webkit-slider-runnable-track {
     width: 100%;
     height: 3px;
+    margin: 5px 0;
     cursor: pointer;
     background: ${(props) => props.theme.colors.primary};
     border-radius: 1.5px;
-  }
-
-  &:focus::-webkit-slider-runnable-track {
-    background: ${(props) => props.theme.colors.primary};
   }
 
   &::-moz-range-track {
@@ -64,7 +51,5 @@ const Input = styled.input`
     border-radius: 1.5px;
   }
 `;
-
-const RangeInput = (props) => <Input type='range' {...props} />;
 
 export default RangeInput;
