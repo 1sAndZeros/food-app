a55642c32518c6597e617e820ba75af25ad9f611
Roberto <roberto.quadraccia90@gmail.com>
2024-03-31 17:03:46 +0200

    Cuisine filtering logic implemented

--- src/app/api/recipes/route.ts
+++ src/app/api/recipes/route.ts
@@ -1,19 +1,25 @@
-import { Recipe } from '@/types';
-import data from '../../../data/recipes.json';
-import { NextResponse } from 'next/server';
+import { Recipe } from "@/types";
+import data from "../../../data/recipes.json";
+import { NextResponse } from "next/server";
 
 export const GET = async (req: any) => {
-  const dishType = req.nextUrl.searchParams.get('dishType');
-  const cuisine = req.nextUrl.searchParams.get('cuisine');
+  const dishType = req.nextUrl.searchParams.get("dishType");
+  const cuisine = req.nextUrl.searchParams.get("cuisine");
   // const dishType = req.nextUrl.searchParams.get('dishType');
   // const dishType = req.nextUrl.searchParams.get('dishType');
   // const dishType = req.nextUrl.searchParams.get('dishType');
-  const { recipes } = JSON.parse(JSON.stringify(data));
+  let { recipes } = JSON.parse(JSON.stringify(data));
   if (dishType) {
-    const filteredRecipes = recipes.filter((recipe: Recipe) =>
-      recipe.dishTypes.includes(dishType.toLowerCase()),
+    recipes = recipes.filter((recipe: Recipe) =>
+      recipe.dishTypes.includes(dishType.toLowerCase())
     );
-    return NextResponse.json({ recipes: filteredRecipes });
+    // return NextResponse.json({ recipes: filteredRecipes });
   }
-  return NextResponse.json(data);
+  if (cuisine) {
+    recipes = recipes.filter((recipe: Recipe) =>
+      recipe.cuisines.includes(cuisine)
+    );
+    // return NextResponse.json({ recipes: filteredRecipes });
+  }
+  return NextResponse.json({ recipes: recipes });
 };
