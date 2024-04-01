2859250f353155d9f29d20eac34e8bd12b42f82b
1sAndZeros <rikie_p@hotmail.com>
2024-04-01 12:54:46 +0100

    added login, create and logout in navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,12 @@
-'use client'
+'use client';
 import styled from 'styled-components';
 import Link from 'next/link';
 import Image from 'next/image';
 import { usePathname } from 'next/navigation';
+import { useSession } from 'next-auth/react';
 import User from './User';
 import { navLinks } from '@/data';
-
+import Button from './Button';
 
 const StyledNavbar = styled.header`
   display: flex;
@@ -19,45 +20,77 @@ const StyledNavbar = styled.header`
     display: flex;
     align-items: center;
     font-weight: 600;
-    gap: .4rem;
+    gap: 0.4rem;
   }
   & nav {
     text-transform: uppercase;
     display: flex;
     list-style: none;
     gap: 1rem;
-  
+
     & a {
-      color: ${props => props.theme.colors.neutral.grey};
-      font-size: clamp(.7rem, 2vw, .8rem);
+      color: ${(props) => props.theme.colors.neutral.grey};
+      font-size: clamp(0.7rem, 2vw, 0.8rem);
     }
     .active {
-        color: ${props => props.theme.colors.neutral.black};
-        border-bottom: 2px solid ${props => props.theme.colors.primary};
-      }
+      color: ${(props) => props.theme.colors.neutral.black};
+      border-bottom: 2px solid ${(props) => props.theme.colors.primary};
+    }
+  }
+  & .auth {
+    display: flex;
+    gap: 1rem;
+    align-items: center;
+    &.user {
+      align-items: center;
+      gap: 0.5rem;
+    }
+    & .create {
+      background-color: black;
+      color: white;
+      padding: 0.5rem 1rem;
+      border-radius: 999px;
+    }
   }
 `;
 
 const Navbar = () => {
   const pathname = usePathname();
+  const { data: session } = useSession();
   const isActive = (link: string) => pathname === link;
 
   return (
-      <StyledNavbar>
-        <Link href='/' className='brand'>
-          <Image alt='logo' src='Logo.svg' width='40' height='30' />
-            Food App
+    <StyledNavbar>
+      <Link href='/' className='brand'>
+        <Image alt='logo' src='Logo.svg' width='40' height='30' />
+        Food App
+      </Link>
+      <nav>
+        {navLinks.map((link, index) => (
+          <li key={index}>
+            <Link
+              className={isActive(link.link) ? 'active' : ''}
+              href={link.link}
+            >
+              {link.name}
+            </Link>
+          </li>
+        ))}
+      </nav>
+      {session && session.user ? (
+        <div className='auth user'>
+          <User />
+          <Link href='/api/auth/signout'>Logout</Link>
+        </div>
+      ) : (
+        <div className='auth'>
+          <Link href='/api/auth/signin'>Login</Link>
+          <Link href='/auth/signup' className='create'>
+            Create Account
           </Link>
-        <nav>
-          {navLinks.map((link, index) => (
-              <li key={index}>
-                  <Link className={isActive(link.link) ? 'active' : ''} href={link.link}>{link.name}
-                  </Link>
-              </li>
-          ))}
-        </nav>
-        <User />
-      </StyledNavbar>
+        </div>
+      )}
+    </StyledNavbar>
   );
 };
 
